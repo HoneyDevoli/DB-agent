6444b6b0ca1ae1e34232d4e80a8c0cc1487c0565
HoneyDevoly <olegwar97@mail.ru>
2018-07-11 16:58:41 +0400

    add creater of databases, test connections to db and getter connections to db.

--- src/main/java/config/PostgreConfig.java
+++ src/main/java/config/PostgreConfig.java
@@ -3,6 +3,7 @@ package config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,56 +11,100 @@ public class PostgreConfig {
 
     private static final String CLASS_NAME = PostgreConfig.class.getName();
     private static final Logger logger = Logger.getLogger(CLASS_NAME);
-    static final String DB_URL = "jdbc:postgresql://localhost:5432/external_db_people";
-    static final String USER = "postgres";
-    static final String PASS = "root";
+
+    private static final String SERVER_URL = "jdbc:postgresql://localhost:5432/";
+    private static final String INNER_DB_URL = "jdbc:postgresql://localhost:5432/internal_people_db";
+    private static final String OUTER_DB_URL = "jdbc:postgresql://localhost:5432/external_people_db";
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String USER = "postgres";
+    private static final String PASS = "pgpassword";
 
     public static void testConnectionToDB() {
-
-        System.out.println("Testing connection to PostgreSQL JDBC");
-
+        final String METHOD_NAME = "testConnectionToDB";
+        logger.log(Level.SEVERE,"Testing connection to PostgreSQL JDBC");
+        Connection connectionServer = null;
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName(DRIVER);
+            logger.log(Level.SEVERE,"PostgreSQL JDBC Driver successfully connected");
+
+            connectionServer = DriverManager.getConnection(SERVER_URL, USER, PASS);
+            logger.log(Level.SEVERE,"You successfully connected to server postgre now");
+
         } catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
-            e.printStackTrace();
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "PostgreSQL JDBC Driver is not found. Include it in your library path", e);
             return;
-        }
-
-        System.out.println("PostgreSQL JDBC Driver successfully connected");
-        Connection connection = null;
-
-        try {
-            connection = DriverManager
-                    .getConnection(DB_URL, USER, PASS);
-
         } catch (SQLException e) {
-            System.out.println("Connection Failed");
-            e.printStackTrace();
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "Failed to make connection to server postgre. Please check properties", e);
             return;
         }
 
-        if (connection != null) {
-            System.out.println("You successfully connected to database now");
-        } else {
-            System.out.println("Failed to make connection to database");
+        try{
+            Connection connection = DriverManager.getConnection(INNER_DB_URL, USER, PASS);
+            logger.log(Level.SEVERE,"You successfully connected to internal db postgre");
+            Statement stmt = connectionServer.createStatement();
+        } catch (SQLException e) {
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "Database \"internal_people_db\" doesn`t exist", e);
+            InitializerDB.CreateInnerDb(connectionServer);
+            return;
         }
+
+        try{
+            Connection connection = DriverManager.getConnection(OUTER_DB_URL, USER, PASS);
+            logger.log(Level.SEVERE,"You successfully connected to external db postgre");
+        } catch (SQLException e) {
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "Database \"external_people_db\" doesn`t exist", e);
+            return;
+        }
+
+
     }
 
-    public static Connection getDBConnection() {
-        final String METHOD_NAME = "getDBConnection";
+    public static Connection getConnectionToInnerDB() {
+        final String METHOD_NAME = "getConnectionToInnerDB";
 
         Connection dbConnection = null;
         try {
-            Class.forName("org.postgresql.Driver");
-            dbConnection = DriverManager.getConnection(DB_URL, USER, PASS);
+            Class.forName(DRIVER);
+            dbConnection = DriverManager.getConnection(INNER_DB_URL, USER, PASS);
             return dbConnection;
         } catch (ClassNotFoundException e) {
-            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "Не удалось загрузить драйвер", e);
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "PostgreSQL JDBC Driver is not found. Include it in your library path", e);
         } catch (SQLException e) {
-            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "Не удалось создать соединение", e);
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "Failed to make connection to Inner DateBase Postgres. Please check properties", e);
         }
         return dbConnection;
+    }
+
+    public static Connection getConnectionToOuterDB() {
+        final String METHOD_NAME = "getConnectionToOuterDB";
+
+        Connection dbConnection = null;
+        try {
+            Class.forName(DRIVER);
+            dbConnection = DriverManager.getConnection(OUTER_DB_URL, USER, PASS);
+            return dbConnection;
+        } catch (ClassNotFoundException e) {
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "PostgreSQL JDBC Driver is not found. Include it in your library path", e);
+        } catch (SQLException e) {
+            logger.logp(Level.SEVERE, CLASS_NAME, METHOD_NAME, "Failed to make connection to Outer DateBase Postgres. Please check properties", e);
+        }
+        return dbConnection;
+    }
+
+    public static Connection getConnectionToServer(){
+        Connection connectionServer = null;
+        try {
+            Class.forName(DRIVER);
+            connectionServer = DriverManager.getConnection(SERVER_URL, USER, PASS);
+            return  connectionServer;
+
+        } catch (ClassNotFoundException e) {
+            logger.log(Level.SEVERE,"PostgreSQL JDBC Driver is not found. Include it in your library path", e);
+            return null;
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE,"Failed to make connection to server postgre. Please check properties", e);
+            return null;
+        }
     }
 }
 
